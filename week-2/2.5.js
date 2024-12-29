7684b4cdd43238a1043c06b73ddc27711ca784ce
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2024-12-30 03:32:26 +0530

    Commit

--- week-2/2.5.js
+++ week-2/2.5.js
@@ -12,10 +12,24 @@ function calculateSum(n){
 // let ans = calculateSum(3);
 // console.log(ans);
 
+function sum( a , b ){
+    const ans = a + b;
+    return ans;
+}
+
 app.get('/', function(req ,res){
     const n = req.query.n;
     const ans = calculateSum(n);
+    res.send(ans.toString());  // res.send expects string, buffer or an object, can't pass number directly
+})
+
+
+app.get('/sum', function(req ,res){
+    const a = req.query.a;
+    const b = req.query.b;
+    const ans = sum(a , b);
     res.send(ans.toString());
 })
 
 app.listen(3000);
+
