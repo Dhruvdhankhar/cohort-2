7ed6e0a08729ad1c56eb2ed86d4614d9ab08994f
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2025-01-01 02:06:58 +0530

    map

--- week-2/mapFilter.js
+++ week-2/mapFilter.js
@@ -1,4 +1,17 @@
+//MAP
+
 // given an arr, multiple each element by 2 to form new Array
+// MENTOS ZINDAGI
+
+const arr = [1,2,3,4,5]
+
+const transform = (i)=>{
+    return i*2;
+}
+// take arr as inpust parameter for transform();
+const result = arr.map(transform);
+console.log(result);
+
 // AAM ZINDAGI -->
 // const ans = add(1,2)
 // console.log(ans);
@@ -11,15 +24,41 @@
 //         newArr.push(arr[i]*2)
 // }
 // console.log(arr);
-
 // console.log(newArr);
+
+
+
+
+// filter
+
+// given an array, print all the even value from the array
 
 // MENTOS ZINDAGI
 
-const arr = [1,2,3,4,5]
+const input = [2,3,4,5,6]
 
-const transform = (i)=>{
-    return i*2;
+const filterLogic = (n) =>{
+    if(n%2 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-const ans = arr.map(transform);  // take arr as inpust parameter for transform();
+
+const ans = input.filter(filterLogic);
 console.log(ans);
+
+//AAM ZINDAGI
+// const input = [1,2,3,4,5,6,7,8]
+
+// const evenInput = []
+// for(let i = 0; i < input.length ; i++){
+//     if(input[i]%2 == 0){
+//         evenInput.push(input[i])
+//     }
+// }
+// console.log(evenInput);
+
+
+
