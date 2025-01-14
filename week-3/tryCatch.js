c32e363aadfb2b6b70ebb3a6f53025addab5de0d
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2025-01-14 22:55:47 +0530

    file change

---
+++ week-3/tryCatch.js
@@ -0,0 +1,12 @@
+// throwing and catching err in js
+
+try{
+    const name = Dhruv
+    const ans = name.length;
+    console.log(ans) 
+}
+ catch(e){
+    console.log("hi from catch");   
+}
+
+console.log("hi");
